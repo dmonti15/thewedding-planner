189b03d486326882eea4686c7b8cc608d74bcc09
Daniella Montinola <dmonti15@gmail.com>
2018-02-13 22:54:36 -0600

    Design updates and Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  get '/home' => 'pages#home'
+  get '/about' => 'pages#about'
+  get '/services' => 'pages#services'
+  get '/contact' => 'pages#contact'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
